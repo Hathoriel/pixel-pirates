ca4448f1435a7cd37e32397fc39b0a3c12cd83f2
Hathoriel <lukas.kotol@gmail.com>
2024-06-03 14:16:05 +0200

    Added tilesets

---
+++ assets/tiles/Back Palm Tree Regular 01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Back Palm Tree Regular 01" tilewidth="16" tileheight="16" tilecount="16" columns="4">
+ <image source="../../../../Downloads/Treasure Hunters/Palm Tree Island/Sprites/Back Palm Trees/Back Palm Tree Regular 01.png" width="64" height="64"/>
+</tileset>
